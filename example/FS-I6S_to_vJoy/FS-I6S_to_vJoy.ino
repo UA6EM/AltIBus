2149d90e1b8f0e731daedd0cde862a2dd031c771
AlexBeem <71223577+AlexBeem@users.noreply.github.com>
2021-11-20 14:10:10 +0300

    Update FS-I6S_to_vJoy.ino

--- example/FS-I6S_to_vJoy/FS-I6S_to_vJoy.ino
+++ example/FS-I6S_to_vJoy/FS-I6S_to_vJoy.ino
@@ -1,1 +1,20 @@
+// Приём IBUS с использованием библиотеки AltSoftSerial.h и передача по протоколу IBUS
+// в стандартный серийный порт (USB)
+// Для Atmega328P
 
+#include <AltSoftSerial.h>
+AltSoftSerial mySerial(8, 9);   // используются только эти пины, другие назначить нельзя
+
+volatile byte ibyte;
+
+void setup() {
+ Serial.begin(115200);
+ mySerial.begin(115200);
+}
+
+void loop() {
+ if(mySerial.available() > 0){
+  ibyte = mySerial.read();
+  Serial.write(ibyte);
+ }
+}
